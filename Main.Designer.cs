e3be8662488d2f942e4606b6b13c06fec517a547
MarioGuilherme <marioguietec@gmail.com>
2022-01-25 23:58:27 -0300

    Adicionar arquivos de projeto.

---
+++ Main.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace CRUD
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputID = new System.Windows.Forms.TextBox();
+            this.inputName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputCost = new System.Windows.Forms.TextBox();
+            this.inputPrice = new System.Windows.Forms.TextBox();
+            this.btnNew = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnConsult = new System.Windows.Forms.Button();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inputID
+            // 
+            this.inputID.Location = new System.Drawing.Point(112, 36);
+            this.inputID.Name = "inputID";
+            this.inputID.Size = new System.Drawing.Size(146, 20);
+            this.inputID.TabIndex = 1;
+            this.inputID.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // inputName
+            // 
+            this.inputName.Location = new System.Drawing.Point(112, 79);
+            this.inputName.Name = "inputName";
+            this.inputName.Size = new System.Drawing.Size(146, 20);
+            this.inputName.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Poppins ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(39, 75);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 28);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Name:";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Poppins ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ID:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(51, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 28);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Cost:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Poppins ExtraBold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 157);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 28);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Price:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // inputCost
+            // 
+            this.inputCost.Location = new System.Drawing.Point(112, 119);
+            this.inputCost.Name = "inputCost";
+            this.inputCost.Size = new System.Drawing.Size(146, 20);
+            this.inputCost.TabIndex = 9;
+            // 
+            // inputPrice
+            // 
+            this.inputPrice.Location = new System.Drawing.Point(112, 161);
+            this.inputPrice.Name = "inputPrice";
+            this.inputPrice.Size = new System.Drawing.Size(146, 20);
+            this.inputPrice.TabIndex = 10;
+            // 
+            // btnNew
+            // 
+            this.btnNew.Font = new System.Drawing.Font("Poppins ExtraBold", 12F);
+            this.btnNew.Location = new System.Drawing.Point(112, 203);
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(106, 35);
+            this.btnNew.TabIndex = 11;
+            this.btnNew.Text = "New";
+            this.btnNew.UseVisualStyleBackColor = true;
+            this.btnNew.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Font = new System.Drawing.Font("Poppins ExtraBold", 12F);
+            this.btnEdit.Location = new System.Drawing.Point(238, 203);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(106, 35);
+            this.btnEdit.TabIndex = 11;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Poppins ExtraBold", 12F);
+            this.btnDelete.Location = new System.Drawing.Point(365, 203);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(106, 35);
+            this.btnDelete.TabIndex = 11;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnConsult
+            // 
+            this.btnConsult.Font = new System.Drawing.Font("Poppins ExtraBold", 12F);
+            this.btnConsult.Location = new System.Drawing.Point(490, 203);
+            this.btnConsult.Name = "btnConsult";
+            this.btnConsult.Size = new System.Drawing.Size(106, 35);
+            this.btnConsult.TabIndex = 11;
+            this.btnConsult.Text = "Consult";
+            this.btnConsult.UseVisualStyleBackColor = true;
+            this.btnConsult.Click += new System.EventHandler(this.btnConsult_Click);
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Poppins ExtraBold", 12F);
+            this.btnDisplay.Location = new System.Drawing.Point(611, 203);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(106, 35);
+            this.btnDisplay.TabIndex = 11;
+            this.btnDisplay.Text = "Display";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(112, 259);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(605, 225);
+            this.dataGrid.TabIndex = 12;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 514);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.btnDisplay);
+            this.Controls.Add(this.btnConsult);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.btnNew);
+            this.Controls.Add(this.inputPrice);
+            this.Controls.Add(this.inputCost);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.inputName);
+            this.Controls.Add(this.inputID);
+            this.Name = "Main";
+            this.Text = "CRUD";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox inputID;
+        private System.Windows.Forms.TextBox inputName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputCost;
+        private System.Windows.Forms.TextBox inputPrice;
+        private System.Windows.Forms.Button btnNew;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnConsult;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,125 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+using System.Data;
+using CRUD.Database;
+
+namespace CRUD {
+    public partial class Main : Form {
+        private static MySqlConnection connection = Database.Database.Connect();
+        private static MySqlCommand command;
+        private static MySqlDataAdapter da;
+        private static MySqlDataReader dr;
+        private string sql;
+
+        public Main() {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {}
+
+        private void label1_Click(object sender, EventArgs e) {}
+
+        private void textBox1_TextChanged(object sender, EventArgs e) {}
+
+        private void label2_Click(object sender, EventArgs e) {}
+
+        private void label3_Click(object sender, EventArgs e) {}
+
+        private void button5_Click(object sender, EventArgs e) {
+            try {
+                connection = new MySqlConnection("Server=localhost;Database=crud_dotnet;Uid=root;Pwd=");
+                sql = "SELECT * FROM products";
+                da = new MySqlDataAdapter(sql, connection);
+                DataTable dataTable = new DataTable();
+                da.Fill(dataTable);
+                dataGrid.DataSource = dataTable;
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                connection.Close();
+                connection = null;
+                command = null;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            try {
+                connection = new MySqlConnection("Server=localhost;Database=crud_dotnet;Uid=root;Pwd=");
+                sql = "INSERT INTO products (name, price, cost) VALUES (@name, @price, @cost)";
+                command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@name", inputName.Text);
+                command.Parameters.AddWithValue("@price", inputPrice.Text);
+                command.Parameters.AddWithValue("@cost", inputCost.Text);
+                connection.Open();
+                command.ExecuteNonQuery();
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                connection.Close();
+                connection = null;
+                command = null;
+            }
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e) {
+            try {
+                connection = new MySqlConnection("Server=localhost;Database=crud_dotnet;Uid=root;Pwd=");
+                sql = "UPDATE products SET name = @name, cost = @cost, price = @price WHERE id_product = @id_product";
+                command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@name", inputName.Text);
+                command.Parameters.AddWithValue("@price", inputPrice.Text);
+                command.Parameters.AddWithValue("@cost", inputCost.Text);
+                command.Parameters.AddWithValue("@id_product", inputID.Text);
+                connection.Open();
+                command.ExecuteNonQuery();
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                connection.Close();
+                connection = null;
+                command = null;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e) {
+            try {
+                connection = new MySqlConnection("Server=localhost;Database=crud_dotnet;Uid=root;Pwd=");
+                sql = "DELETE FROM products WHERE id_product = @id_product";
+                command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@id_product", inputID.Text);
+                connection.Open();
+                command.ExecuteNonQuery();
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                connection.Close();
+                connection = null;
+                command = null;
+            }
+        }
+
+        private void btnConsult_Click(object sender, EventArgs e) {
+            try {
+                connection = new MySqlConnection("Server=localhost;Database=crud_dotnet;Uid=root;Pwd=");
+                sql = "SELECT * FROM products WHERE id_product = @id_product";
+                command = new MySqlCommand(sql, connection);
+                command.Parameters.AddWithValue("@id_product", inputID.Text);
+                connection.Open();
+                dr = command.ExecuteReader();
+                while(dr.Read()) {
+                    inputID.Text = Convert.ToString(dr["id_product"]);
+                    inputName.Text = Convert.ToString(dr["name"]);
+                    inputCost.Text = Convert.ToString(dr["cost"]);
+                    inputPrice.Text = Convert.ToString(dr["price"]);
+                }
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message);
+            } finally {
+                connection.Close();
+                connection = null;
+                command = null;
+            }
+        }
+    }
+}
